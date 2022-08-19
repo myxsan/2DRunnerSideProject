c735838e764fc7ee62906b63afde9c745cc7ba81
myxsan <yksl3461@gmail.com>
2022-08-19 14:43:14 +0300

    q

--- 2DRunnerSideProject/Assets/Scripts/PlayerController.cs
+++ 2DRunnerSideProject/Assets/Scripts/PlayerController.cs
@@ -9,12 +9,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] GameObject fireballs;
 
     private Rigidbody2D playerRigidbody;
-    private BoxCollider2D playerCollider;
     private Animator playerAnimator;
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
-        playerCollider = GetComponent<BoxCollider2D>();
         playerAnimator = GetComponent<Animator>();
 
         fireballs.SetActive(false);
@@ -34,25 +32,10 @@ public class PlayerController : MonoBehaviour
             fireballs.SetActive(true);
             playerAnimator.SetBool("IsFlying", true);
         }
-        else if ((!Input.GetKey(KeyCode.Space) || !Input.GetMouseButton(0)) && !HasVerticleSpeed())
-        {
-            playerAnimator.SetBool("IsFlying", false);
-        }
         else if (Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0))
         {
+            playerAnimator.SetBool("IsFlying", false);
             fireballs.SetActive(false);
-        }
-    }
-
-    private bool HasVerticleSpeed()
-    {
-        if (playerRigidbody.velocity.y < 0.0001f && playerRigidbody.velocity.y > -0.0001f && transform.position.y < -4.5)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
         }
     }
 }
